2a221e04ed562f12395473b75cf4dec2ef9e9ee6
Aaron Mertzenich <mertzi52101@hotmail.com>
2018-11-09 07:55:49 -0600

    Always register service worker

--- src/main.jsx
+++ src/main.jsx
@@ -564,13 +564,8 @@ class App extends Component {
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-if (navigator.serviceWorker.controller) {
-    console.log('[PWA Builder] active service worker found, no need to register')
-} else {
-    //Register the ServiceWorker
-    navigator.serviceWorker.register('/service-worker.js', {
-        scope: './'
-    }).then(function(reg) {
-        console.log('Service worker has been registered for scope:'+ reg.scope);
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');
     });
 }
