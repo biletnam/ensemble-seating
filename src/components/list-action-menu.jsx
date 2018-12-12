8fe76f4f43d92435cd20cf9aa34cdeba8675c450
Aaron Mertzenich <mertzi52101@hotmail.com>
2018-12-11 22:56:42 -0600

    Keep Region context menu on top of other headings Resolves a z-index issue where sticky headings showed above menu dialog

--- src/components/list-action-menu.jsx
+++ src/components/list-action-menu.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './list-action-menu.css';
 
-import { SimpleMenu, MenuItem } from '@rmwc/menu';
+import { Menu, MenuItem, MenuSurfaceAnchor } from '@rmwc/menu';
 import '@material/menu/dist/mdc.menu.css';
 import '@material/menu-surface/dist/mdc.menu-surface.css';
 import '@material/list/dist/mdc.list.css';
@@ -22,16 +22,19 @@ class ListActionMenu extends React.Component {
 
     render() {
         const { onSelectAction, ...props } = this.props;
-        return <SimpleMenu {...props}>
-            <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='edit'>Edit&hellip;</MenuItem>
-            <hr className='list-action-menu__divider' />
-            <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-to-top'>Move to top</MenuItem>
-            <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-up'>Move up</MenuItem>
-            <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-down'>Move down</MenuItem>
-            <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-to-bottom'>Move to bottom</MenuItem>
-            <hr className='list-action-menu__divider' />
-            <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='delete'>Delete</MenuItem>
-        </SimpleMenu>
+        return <MenuSurfaceAnchor>
+            <Menu {...props}>
+                <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='edit'>Edit&hellip;</MenuItem>
+                <hr className='list-action-menu__divider' />
+                <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-to-top'>Move to top</MenuItem>
+                <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-up'>Move up</MenuItem>
+                <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-down'>Move down</MenuItem>
+                <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='move-to-bottom'>Move to bottom</MenuItem>
+                <hr className='list-action-menu__divider' />
+                <MenuItem onClick={this.handleClick} className='list-action-menu__item' data-action-type='delete'>Delete</MenuItem>
+            </Menu>
+            {this.props.children}
+        </MenuSurfaceAnchor>
     }
 }
 
