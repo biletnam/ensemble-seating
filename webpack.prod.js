3a219e844930bcb904708571c9d7831f255a67dd
Aaron Mertzenich <aaron.mertzenich@gmail.com>
2019-05-04 09:01:19 -0500

    v0.10.1 Removes Webpack externals to fix a bug with CDN URLs

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
-
-const externalAttributes = {
-    crossorigin: true
-};
 
 module.exports = merge(common, {
     mode: 'production',
@@ -14,35 +9,6 @@ module.exports = merge(common, {
         compress: true
     },
     devtool: 'source-map',
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM",
-        "react-beautiful-dnd": 'ReactBeautifulDnd',
-        'workbox-window': 'window.workbox'
-    },
-    plugins: [
-        new HtmlWebpackIncludeAssetsPlugin({
-            assets: [
-                {
-                    path: 'https://unpkg.com/react@16.8.6/umd/react.production.min.js',
-                    attributes: externalAttributes
-                },
-                {
-                    path: 'https://unpkg.com/react-dom@16.8.6/umd/react-dom.production.min.js',
-                    attributes: externalAttributes
-                },
-                {
-                    path: 'https://unpkg.com/react-beautiful-dnd@11.0.1/dist/react-beautiful-dnd.min.js',
-                    attributes: externalAttributes
-                },
-                {
-                    path: 'https://unpkg.com/workbox-window@4.3.0/build/workbox-window.prod.umd.js',
-                    attributes: externalAttributes
-                }
-            ],
-            append: false
-        })
-    ],
     optimization: {
         minimizer: [
             new TerserPlugin({
