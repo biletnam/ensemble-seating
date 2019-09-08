c699bb659128528984ce681202ecd4886fc157dd
Aaron Mertzenich <aaron.mertzenich@gmail.com>
2019-09-08 14:46:19 -0500

    Create IntegerInput component for row settings

--- src/components/edit-section.jsx
+++ src/components/edit-section.jsx
@@ -6,14 +6,16 @@ import { TwitterPicker } from 'react-color';
 
 import { TextField } from '@rmwc/textfield';
 import { Button } from '@rmwc/button';
-import { IconButton } from '@rmwc/icon-button';
 import { Radio } from '@rmwc/radio';
 
 import '@material/textfield/dist/mdc.textfield.min.css';
 import '@material/button/dist/mdc.button.min.css';
-import '@material/icon-button/dist/mdc.icon-button.min.css';
 import '@material/radio/dist/mdc.radio.min.css';
 import '@material/form-field/dist/mdc.form-field.min.css';
+
+import IntegerInput from './integer-input.jsx';
+
+import './edit-section.css';
 
 import ClearIcon from '../icons/baseline-clear-24px.jsx';
 
@@ -21,9 +23,7 @@ class SectionEditor extends PureComponent {
     constructor(props) {
         super(props);
 
-        this.handleChange = this.handleChange.bind(this);
         this.handleColorChange = this.handleColorChange.bind(this);
-        this.handleRowBlur = this.handleRowBlur.bind(this);
         this.handleClickedAddRow = this.handleClickedAddRow.bind(this);
         this.handleClickedRemoveRow = this.handleClickedRemoveRow.bind(this);
     }
@@ -57,42 +57,8 @@ class SectionEditor extends PureComponent {
             this.props.onRequestEdit(this.props.data.id, saveData);
     }
 
-    handleChange(event) {
-        const settingType = event.target.dataset.settingType;
-        if (settingType === 'name') {
-            this.updateSectionName(event.target.value);
-        }
-
-        else if (settingType === 'rowSettings') {
-            const rowIndex = parseInt(event.target.getAttribute('data-row'), 10);
-            this.updateRowSetting(rowIndex, event.target.value);
-        }
-
-        else if (settingType === 'offsetType') {
-            this.updateSectionOffsetType(event.target.value);
-        }
-
-        else if (settingType === 'offsetValue') {
-            this.updateSectionOffsetValue(event.target.value);
-        }
-    }
-
     handleColorChange(color, event) {
         this.updateSectionColor(color.hex);
-    }
-
-    handleRowBlur(event) {
-        const settingType = event.target.dataset.settingType;
-
-        const valueAsInt = parseInt(event.target.value, 10);
-        if (settingType === 'rowSettings') {
-            const rowIndex = parseInt(event.target.getAttribute('data-row'), 10);
-            this.updateRowSetting(rowIndex, isNaN(valueAsInt) ? 0 : valueAsInt);
-        }
-
-        else if (settingType === 'offsetValue') {
-            this.updateSectionOffsetValue(isNaN(valueAsInt) ? 0 : valueAsInt);
-        }
     }
 
     handleClickedAddRow(event) {
@@ -104,10 +70,9 @@ class SectionEditor extends PureComponent {
             this.props.onRequestEdit(this.props.data.id, saveData);
     }
 
-    handleClickedRemoveRow(event) {
+    handleClickedRemoveRow(row) {
         if (this.props.data.rowSettings.length > 1) {
             const saveData = {};
-            const row = parseInt(event.target.getAttribute('data-row'));
             saveData.rowSettings = this.props.data.rowSettings.slice();
             saveData.rowSettings.splice(row, 1);
 
@@ -119,9 +84,9 @@ class SectionEditor extends PureComponent {
     render() {
         const {data, onRequestEdit, ...rest} = this.props;
         return <div {...rest}>
-            {data && <React.Fragment>
+            {data && <>
                 <div>
-                    <TextField label='Name' name='name' data-setting-type='name' value={data.name} onChange={this.handleChange} />
+                    <TextField label='Name' name='name' data-setting-type='name' value={data.name} onChange={event => this.updateSectionName(event.target.value)} />
                 </div>
                 
                 <div>
@@ -132,12 +97,11 @@ class SectionEditor extends PureComponent {
                 <div>
                     <h2>Performers per row</h2>
                     <p>Pick how many performers may be seated within each row of this section.</p>
-                {data.rowSettings.map((current, index) => <div key={index + '-rowSettings'} className='text-input-wrapper'>
-                    <TextField label={`Row ${index + 1}`} data-row={index} data-setting-type='rowSettings' value={current} onChange={this.handleChange} onBlur={this.handleRowBlur} />
-                    <IconButton icon={<ClearIcon />} label='Remove row' onClick={this.handleClickedRemoveRow} data-row={index} />
-                </div>)}
-                <br />
-                    <Button onClick={this.handleClickedAddRow}>Add row</Button>
+                    {data.rowSettings.map((current, index) => <div key={index + '-rowSettings'} className='section-editor__row-setting-container'>
+                        <IntegerInput label={`Row ${index + 1}`} min={0} value={current} onChange={value => {this.updateRowSetting(index, value)}} />
+                        <Button className='text-input-wrapper' label='Remove row' icon={<ClearIcon />} onClick={() => this.handleClickedRemoveRow(index)} />
+                    </div>)}
+                    <Button outlined onClick={this.handleClickedAddRow} raised>Add row</Button>
                 </div>
 
                 <div>
@@ -146,26 +110,26 @@ class SectionEditor extends PureComponent {
                     <div>
                         <Radio value='first-row' name='offsetType'
                             data-setting-type='offsetType'
-                            onChange={this.handleChange}
+                            onChange={event => this.updateSectionOffsetType(event.target.value)}
                             checked={data.offsetType === 'first-row'}>Front of ensemble</Radio><br />
+
                         <Radio value='custom-row' name='offsetType'
                             data-setting-type='offsetType'
-                            onChange={this.handleChange}
+                            onChange={event => this.updateSectionOffsetType(event.target.value)}
                             checked={data.offsetType === 'custom-row'}>Start on row:</Radio><br />
-                        <TextField pattern='\d+' disabled={data.offsetType !== 'custom-row'}
-                            data-setting-type='offsetValue'
-                            onChange={this.handleChange}
-                            onBlur={this.handleRowBlur}
-                            value={data.offsetValue} /><br />
+
+                        <IntegerInput disabled={data.offsetType !== 'custom-row'}
+                            value={data.offsetValue}
+                            min={0}
+                            onChange={value => this.updateSectionOffsetValue(value)} />
 
                         <Radio value='last-row' name='offsetType'
                             data-setting-type='offsetType'
-                            onChange={this.handleChange}
+                            onChange={event => this.updateSectionOffsetType(event.target.value)}
                             checked={data.offsetType === 'last-row'}>Back of ensemble</Radio>
                     </div>
                 </div>
-            </React.Fragment>}
-            
+            </>}
         </div>
     }
 }
---
+++ src/components/integer-input.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { TextField } from '@rmwc/textfield';
+import '@material/textfield/dist/mdc.textfield.css';
+import '@material/floating-label/dist/mdc.floating-label.css';
+import '@material/notched-outline/dist/mdc.notched-outline.css';
+import '@material/line-ripple/dist/mdc.line-ripple.css';
+
+import { IconButton } from '@rmwc/icon-button';
+import '@material/icon-button/dist/mdc.icon-button.css';
+
+import './integer-input.css';
+
+import AddIcon from '../icons/baseline-add-24px.jsx';
+import RemoveIcon from '../icons/baseline-remove-24px.jsx';
+
+const IntegerInput = props => {
+    const {onChange, min, max, className, ...rest} = props;
+
+    function handleChange (event) {
+        const valueAsInteger = parseInt(event.target.value, 10);
+        onChange(isNaN(valueAsInteger) ? 0 : clamp(valueAsInteger));
+    }
+
+    function clamp (value) {
+        let result = value;
+        if (typeof min == 'number' && value < min)
+            result = min;
+        else if (typeof max == 'number' && value > max)
+            result = max;
+        return result;
+    }
+
+    function decrement () {
+        onChange(clamp(props.value - 1));
+    }
+
+    function increment () {
+        onChange(clamp(props.value + 1));
+    }
+
+    function handleButton (event) {
+        if (event.target.dataset.direction == 'up')
+            increment();
+        else
+            decrement();
+    }
+
+    function handleKeyDown (event) {
+        if (event.keyCode == 38)
+            increment();
+        else if (event.keyCode == 40)
+            decrement();
+    }
+
+    return <div className='integer-input'>
+        <IconButton data-direction='down' onClick={handleButton} disabled={props.disabled || (typeof min == 'number' && props.value <= min)} icon={<RemoveIcon />} />
+        <TextField {...rest} className={`integer-input__text-field${className ? className : ''}`}
+            onChange={handleChange} onKeyDown={handleKeyDown} disabled={props.disabled} />
+        <IconButton data-direction='up' onClick={handleButton} disabled={props.disabled} icon={<AddIcon />} />
+    </div>
+}
+
+export default IntegerInput;
